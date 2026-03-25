ca7ab03972b076f0a48b50a1b1f84cef3cd954d7
Gabriel <gabrieloliveira0099@gmail.com>
2026-03-25 18:00:02 -0300

    feat: study loops - week 2

---
+++ Semana 2/Ciclos/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ciclos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Ciclo while
+            //int valor = 0;
+
+            //while (valor < 10)
+            //{
+            //    Console.WriteLine($"valor: {valor}");
+            //    valor ++;
+            //}
+            #endregion
+
+            #region Ciclo do-while
+            //int num;
+            //do
+            //{
+            //    Console.Write("Digite o valor de num: ");
+            //    num = int.Parse(Console.ReadLine());
+            //} while (num < 10);
+            #endregion
+
+            #region Ciclo for
+
+            //for (int i = 0, j = 5; i < 10; i++, j--)
+            //{
+            //    Console.WriteLine($"i = {i} | j = {j}");
+            //}
+
+            #endregion
+
+            #region foreach
+
+            string[] nomes = { "Gabriel", "Eduarda", "Lucas", "João" };
+
+            int id = 1;
+
+            foreach (var item in nomes)
+            {
+                Console.WriteLine($"Nome: {item} ID: {id}");
+                id++;
+            }
+
+            #endregion
+        }
+    }
+}
---
+++ Semana 2/Ex Fibonacci/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 1;
+            int seq = 0;
+
+            Console.Write("Quantos valores: ");
+            int valores = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Sequência de Fibonacci com {valores} valores:");
+            
+            for (int i = 0; i < valores; i++)
+            {
+                if (i == valores - 1) Console.Write($"{num1}");
+               
+                else Console.Write($"{num1}, ");
+                
+                seq = num1 + num2;
+
+                num1 = num2;
+                num2 = seq;
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
