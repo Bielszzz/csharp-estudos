c43b5d014e73c6fac8a6a515bb4e2ac507e81f48
Gabriel <gabrieloliveira0099@gmail.com>
2026-03-27 19:10:12 -0300

    feat: add constructors, overloading and static study - week 3

--- Semana 3/MetodoConstrutor/Pessoa.cs
+++ Semana 3/MetodoConstrutor/Pessoa.cs
@@ -12,5 +12,35 @@ namespace MetodoConstrutor
         public string sobrenome;
         public int anoNascimento;
         public int idade;
+
+        public Pessoa()
+        {
+            nome = "Desconhecido";
+            sobrenome = "Nenhum";
+            anoNascimento = 0;
+            idade = 0;
+        }
+
+        public Pessoa(string nome, string sobrenome, int anoNascimento)
+        {
+            this.nome = nome;
+            this.sobrenome = sobrenome;
+            this.anoNascimento = anoNascimento;
+            idade = Idade(); // sobrecarga de construtor
+        }
+
+        public Pessoa(string nome, string sobrenome)
+        {
+            this.nome = nome;
+            this.sobrenome = sobrenome;
+            this.anoNascimento = 2008;
+            idade = Idade(); // sobrecarga de construtor
+        }
+
+        private int Idade() // sobrecarga de construtor
+        {
+            return 2026 - anoNascimento;
+        }
+
     }
 }
